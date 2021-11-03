9004f399e45187afb42982c54c690066099c8636
Victoria Sampalo Garcia <vikytasampalo@gmail.com>
2021-11-03 14:45:18 +0100

    Se sigue el codigo

--- src/main/java/juegodel7/JuegoDel7.java
+++ src/main/java/juegodel7/JuegoDel7.java
@@ -1,20 +1,60 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Planteo un menú 
  */
 package juegodel7;
 
-/**
- *
- * @author victoria
- */
+import java.util.Random;
+import java.util.Scanner;
+
 public class JuegoDel7 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        //Declaro objeto de clase tipo Random
+        Random miAleatorio = new Random();
+
+        //------VARIABLES A USAR--------------------
+        int numJ1, numJ2, sumaTotal;
+
+        
+        
+        //5 rondas
+        for (int i = 0; i < 5; i++) {
+            
+        
+        //CONTADOR DE PUNTOS PUNTO 1 
+        //CONTADOR PUNTO ++
+        
+        //Imprimo numeros aleatorios de ambos jugadores
+        //jugador 1
+        //Rango de impresion de 1 a 5
+        
+        
+        numJ1 = miAleatorio.nextInt(5) + 1;
+
+        System.out.println("Jugador 1: " + numJ1);
+        //jugador 2
+
+        numJ2 = miAleatorio.nextInt(5) + 1;
+
+        System.out.println("Jugador 2:" + numJ2);
+
+        //sumamos ambos números 
+        sumaTotal = numJ1 + numJ2;
+
+        // condiciones 
+        if (sumaTotal < 7) {
+            System.out.println("gana J1");
+        } else {
+            System.out.println("gana j2");
+        }
+        if (sumaTotal == 7) {
+            
+            System.out.println("no gana ninguna");
+            i--; //hace que la ronda no cuente 
+        }
+        
+      }
     }
-    
+
 }
